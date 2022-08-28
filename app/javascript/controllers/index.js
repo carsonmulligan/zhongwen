15f06a02fb665b9200f4cbcc0be2ce33388f3394
carson mulligan <carsonmulligan45@gmail.com>
2022-08-28 11:36:39 -0500

    remove angulkar pkg

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,10 +3,3 @@ import controllers from './**/*_controller.js'
 controllers.forEach((controller) => {
   application.register(controller.name, controller.module.default)
 })
-
-
-import { NgPiaoLiangHanziModule } from 'ng-piao-liang-hanzi';
-@NgModule({
-  imports: [NgPiaoLiangHanziModule]
-})
-export class AppModule { }
